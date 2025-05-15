cb156c3c8ee5ce991101cffa34fdcf7ef3869c03
werley.souza <werley.souza@maxdata.com.br>
2025-05-15 17:13:53 -0300

    feature/cart

--- src/Ambev.DeveloperEvaluation.Application/Carts/UpdateCart/UpdateCartItemValidator.cs
+++ src/Ambev.DeveloperEvaluation.Application/Carts/UpdateCart/UpdateCartItemValidator.cs
@@ -14,10 +14,6 @@ namespace Ambev.DeveloperEvaluation.Application.Carts.UpdateCart
                 .GreaterThan(0).WithMessage("Quantity must be greater than zero")
                 .LessThanOrEqualTo(20).WithMessage("The quantity must be greater than zero and less than or equal to twenty");
 
-            RuleFor(ci => ci.UnitPrice)
-                .GreaterThan(0M).WithMessage("Unit price must be greater than zero")
-                .LessThanOrEqualTo(999999.99M).WithMessage("The unit price is invalid");
-
             RuleFor(ci => ci.Discount)
                 .GreaterThanOrEqualTo(0M).WithMessage("The discount cannot be less than zero")
                 .LessThan(100M).WithMessage("The discount cannot be greater than one hundred");
--- src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/UpdateCart/UpdateCartItemResponse.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/UpdateCart/UpdateCartItemResponse.cs
@@ -7,4 +7,7 @@ public class UpdateCartItemResponse
 {
     public Guid ProductId { get; set; }
     public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Discount { get; set; }
+    public decimal TotalPrice { get; private set; }
 }
